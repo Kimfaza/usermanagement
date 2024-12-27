a68e79b7dcf17e15978983430956572c1e35656f
Kimfaza <fauzakhoirunisa268@gmail.com>
2024-12-27 14:56:00 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,29 +1,59 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import "bootstrap/dist/css/bootstrap.min.css";
-import './App.css';
-import Dashboard from './pages/Dashboard';
-import Header from './components/Header';
-import Sidebar from './components/Sidebar';
+import React, { Suspense, useEffect } from 'react'
+import { HashRouter, Route, Routes } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
+import { CSpinner, useColorModes } from '@coreui/react'
+import './scss/style.scss'
 
-function App() {
+// We use those styles to show code examples, you should remove them in your application.
+import './scss/examples.scss'
+
+// Containers
+const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'))
+
+// Pages
+const Login = React.lazy(() => import('./views/pages/login/Login'))
+const Register = React.lazy(() => import('./views/pages/register/Register'))
+const Page404 = React.lazy(() => import('./views/pages/page404/Page404'))
+const Page500 = React.lazy(() => import('./views/pages/page500/Page500'))
+
+const App = () => {
+  const { isColorModeSet, setColorMode } = useColorModes('coreui-free-react-admin-template-theme')
+  const storedTheme = useSelector((state) => state.theme)
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.href.split('?')[1])
+    const theme = urlParams.get('theme') && urlParams.get('theme').match(/^[A-Za-z0-9\s]+/)[0]
+    if (theme) {
+      setColorMode(theme)
+    }
+
+    if (isColorModeSet()) {
+      return
+    }
+
+    setColorMode(storedTheme)
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps
+
   return (
-    <BrowserRouter>
-      <Header/>
-      <div className='main d-flex'>
-        <div className='sidebarWrapper'>
-        <Sidebar/>
-        </div>
-
-        <div className='content'>
+    <HashRouter>
+      <Suspense
+        fallback={
+          <div className="pt-3 text-center">
+            <CSpinner color="primary" variant="grow" />
+          </div>
+        }
+      >
         <Routes>
-        <Route path="/" exact={true} element={<Dashboard/>}/>
-        <Route path="/dashboard" exact={true} element={<Dashboard/>}/>
-      </Routes>
-        </div> 
-      </div>
-    </BrowserRouter>
-  );
+          <Route exact path="/login" name="Login Page" element={<Login />} />
+          <Route exact path="/register" name="Register Page" element={<Register />} />
+          <Route exact path="/404" name="Page 404" element={<Page404 />} />
+          <Route exact path="/500" name="Page 500" element={<Page500 />} />
+          <Route path="*" name="Home" element={<DefaultLayout />} />
+        </Routes>
+      </Suspense>
+    </HashRouter>
+  )
 }
 
-export default App;
+export default App
